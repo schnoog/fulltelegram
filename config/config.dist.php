dab2f067f1ce8e3d7e3a1710a3655313ebbb1747
schnoog <braun.volker@gmail.com>
2018-03-23 19:21:01 +0100

    puh

---
+++ config/config.dist.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$Config['DB'] = array(
+                    'host' => '',
+                    'port' => '',
+                    'user' => '',
+                    'pass' => '',
+                    'name' => '',
+                );
+                
+$Config['Telegram'] = array(
+                    'token' => '',
+                    'botname' => '',
+                );                
+                
